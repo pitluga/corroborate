7085d9b258f37b988690934e983cb77d96812dbd
Tony Pitluga <tony.pitluga@gmail.com>
2011-09-05 07:57:36 -0500

    validate more than one field in a hash

--- spec/validations/core_spec.clj
+++ spec/validations/core_spec.clj
@@ -3,10 +3,15 @@
         [validations.core]))
 
 (describe "validate"
-  (it "validates a simple map"
+  (it "can take an anonymous validation"
     (let [simple {:a "hello"}
           errors (validate simple :a #(if (not (= "goodbye" %)) "invalid"))]
-      (should= {:a ["invalid"]} errors))))
+      (should= {:a ["invalid"]} errors)))
+
+  (it "can validate more than one field"
+    (let [example {}
+          errors (validate {} :a is-required :b is-required)]
+      (should= {:a ["is required"] :b ["is required"] } errors))))
 
 (describe "is-required"
   (it "returns a validation error if the field is missing"
--- src/validations/core.clj
+++ src/validations/core.clj
@@ -1,9 +1,12 @@
 (ns validations.core
   (:use [clojure.contrib.string :only [blank?]]))
 
-(defn validate [topic field validation]
-  (let [error (validation (topic field))]
-    (if (blank? error) {} {field [error]})))
+(defn validate [topic & fields-with-validations]
+  (loop [[field validation & more] fields-with-validations errors {}]
+    (if (nil? field)
+      errors
+      (let [error (validation (field topic))]
+        (recur more (if (blank? error) errors (conj errors [field [error]])))))))
 
 (defn is-required [field]
   (if (blank? field) "is required"))
