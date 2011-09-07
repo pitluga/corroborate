865a065888879972a07c0bbb6e5cc0410176c57a
Tony Pitluga <tony.pitluga@gmail.com>
2011-09-06 21:10:18 -0500

    minimal documentation and we are ready for 0.0.1

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject corroborate "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [
-    [org.clojure/clojure "1.2.1"]
-    [org.clojure/clojure-contrib "1.2.0"]
-    [match "0.2.0-SNAPSHOT"]]
+(defproject corroborate "0.0.1-SNAPSHOT"
+  :description "dead-simple validations"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[speclj "1.2.0"]]
   :test-path "spec/")
