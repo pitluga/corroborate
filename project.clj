f0d695f34ed6436453ad2795f80a231f6bcac7a0
Tony Pitluga <tony.pitluga@gmail.com>
2013-09-10 00:19:28 +0000

    upgrade clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject corroborate "0.3.0-SNAPSHOT"
   :description "dead-simple validations"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
